acdc11284d728baddf88dd9daebdf612915a1349
Keks <keks@htmlacademy.ru>
2025-09-20 17:44:39 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,2 @@
+import './data.js';
+import './photos.js';
---
+++ 6/js/photos.js
@@ -0,0 +1,24 @@
+import { photos } from './data';
+const photosArr = photos();
+const container = document.querySelector('.pictures');
+const fragment = document.createDocumentFragment();
+
+const photosInfo = function() {
+  photosArr.forEach((photo) => {
+    const template = document.querySelector('#picture').content.cloneNode(true);
+    const pictureInfo = template.querySelector('.picture__img');
+    const pictureComments = template.querySelector('.picture__comments');
+    const pictureLikes = template.querySelector('.picture__likes');
+
+    pictureInfo.src = photo.url;
+    pictureInfo.alt = photo.description;
+    pictureLikes.textContent = photo.likes;
+    pictureComments.textContent = photo.comments.length;
+
+    fragment.append(template);
+  });
+  container.append(fragment);
+};
+photosInfo();
+
+export {photosInfo};
